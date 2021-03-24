a7d5210988b74ad3e1d17e0ab8f38672810082bf
Nicolas Rose <nickfbp@gmail.com>
2021-03-24 12:22:00 -0600

    Filter Componet

---
+++ src/components/Filter.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class Filter extends Component {
+  render() {
+    return (
+      <div className="filter">
+        <div className="filter-result">{this.props.count} Products</div>
+        <div className="filter-sort">
+          Order{" "}
+          <select value={this.props.sort} onChange={this.props.sortProducts}>
+            <option>Latest</option>
+            <option value="lowest">Lowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </div>
+        <div className="filter-size">
+          Filter{" "}
+          <select value={this.props.size} onChange={this.props.filterProducts}>
+            <option value="">All</option>
+            <option value="newborn">Newborn</option>
+            <option value="baby">Baby</option>
+            <option value="toddler">Toddler</option>
+            <option value="child">Child</option>
+            <option value="teen">Teen</option>
+            <option value="adult">Adult</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
+}
