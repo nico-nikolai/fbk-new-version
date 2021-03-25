527e82b8fb2d872dabedc6bdc9b259ea16242380
Nicolas Rose <nickfbp@gmail.com>
2021-03-25 13:46:26 -0600

    Animation and Modals

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import formatCurrency from "../util";
+import Fade from "react-reveal/Fade";
 
 export default class Cart extends Component {
   constructor(props) {
@@ -41,27 +42,29 @@ export default class Cart extends Component {
           </div>
         )}
         <div className="cart">
-          <ul className="cart-items">
-            {cartItems.map((item) => (
-              <li key={cartItems._id}>
-                <div>
-                  <img src={item.image} alt={item.title} />
-                </div>
-                <div>
-                  <div>{item.title}</div>
-                  <div className="right">
-                    {formatCurrency(item.price)} x {item.count}{" "}
-                    <button
-                      className="button"
-                      onClick={() => this.props.removeFromCart(item)}
-                    >
-                      Remove
-                    </button>
+          <Fade left cascade>
+            <ul className="cart-items">
+              {cartItems.map((item) => (
+                <li key={cartItems._id}>
+                  <div>
+                    <img src={item.image} alt={item.title} />
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                  <div>
+                    <div>{item.title}</div>
+                    <div className="right">
+                      {formatCurrency(item.price)} x {item.count}{" "}
+                      <button
+                        className="button"
+                        onClick={() => this.props.removeFromCart(item)}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </Fade>
         </div>
         {cartItems.length !== 0 && (
           <div>
@@ -84,9 +87,10 @@ export default class Cart extends Component {
               </div>
             </div>
             {this.state.showCheckout && (
-              <div className="cart">
-                <form onSubmit={this.createOrder}>
-                <ul className="form-container">
+              <Fade right cascade>
+                <div className="cart">
+                  <form onSubmit={this.createOrder}>
+                    <ul className="form-container">
                       <li>
                         <label>Email</label>
                         <input
@@ -120,8 +124,9 @@ export default class Cart extends Component {
                         </button>
                       </li>
                     </ul>
-                </form>
-              </div>
+                  </form>
+                </div>
+              </Fade>
             )}
           </div>
         )}
