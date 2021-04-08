0bea99fc2c37707c559f07f647b93abec95ba293
Nicolas Rose <nickfbp@gmail.com>
2021-04-08 11:49:35 -0600

    Added Font Awesome and fixed links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
-const headerStyle = {
-
-}
+import { Jumbotron } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStore, faBlog } from "@fortawesome/free-solid-svg-icons";
 
 export default class Header extends Component {
   render() {
     return (
-      <div style={{headerStyle}}>
-        <Link to="/home" onMouseOver="this.style.cursor='pointer'">
-          Fat Betty Knits
-          <div className="logo">
-        <img style={{height: "5rem", width: "5rem"}} src="/images/fbk-logo1.jpg" alt="Fat Betty Knits Logo"/>
-        </div>
+      <div className="header-content">
+        <Jumbotron fluid>
+          <Link to="/home" onMouseOver="this.style.cursor='pointer'">
+            <div className="logo">
+              <img
+                style={{ height: "10rem", width: "10rem" }}
+                src="/images/fbk-logo1.jpg"
+                alt="Fat Betty Knits Logo"
+              />
+            </div>
+          </Link>
+        </Jumbotron>
+        <Link to="/our-story">
+          <i className="fa fa-book fa-lg" /> Our Story
         </Link>
-
-        <Link to="/our-story">Our Story</Link>
-        <Link to="/store">Store</Link>
+        <Link to="/store">
+          <FontAwesomeIcon icon={faStore} />
+          <i className="fa fa-store fa-lg" /> Store
+        </Link>
+        <Link to="/blogs">
+          <FontAwesomeIcon icon={faBlog} />
+          <i className="fa fa-blog fa-lg" /> Blogs
+        </Link>
         <Link to="/admin" hidden>
           Admin
         </Link>
-        <Link to="/blogs">Blogs</Link>
       </div>
     );
   }
 }
-
