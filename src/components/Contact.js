5b0269c1c85faf414dbd49ced0f665a3e63d4746
Nicolas Rose <nickfbp@gmail.com>
2021-04-12 19:00:59 -0600

    Fixed contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Zoom from "react-reveal/Zoom";
-
+import Fade from "react-reveal/Fade";
 import { connect } from "react-redux";
 import { createContact, clearContact } from "../actions/contactActions";
 
@@ -37,23 +37,41 @@ class Contact extends Component {
     return (
       <div>
         <Zoom>
-          <div className="">
+          <div className="our">
             <h2>Contact Us!</h2>
             <h3>Ask a Question!</h3>
             <p>
               Enter your info and question below and we'll get right back to
               you.
             </p>
+            </div>
+            </Zoom>
 
-            <ul>
-              <li>
-                <div>Name:</div>
-                <div>{contact.name}</div>
-              </li>
-            </ul>
+            <Fade>
+              <form>
+                <ul className="form-container">
+                  <li>
+                    <label>Name</label>
+                    <input
+                      name="name"
+                      type="text"
+                      required
+                      onChange={this.handleInput}
+                    ></input>
+                  </li>
+                  <li>
+                      <label>Email</label>
+                      <input name="email" type="email" required onChange={this.handleInput}></input>
+                  </li>
+                  <li>
+                      <label>Question</label>
+                      <input name="question" type="textbox" required onChange={this.handleInput}></input>
+                  </li>
+                </ul>
+              </form>
+            </Fade>
 
-          </div>
-        </Zoom>
+
       </div>
     );
   }
